294e05b6b82d95bbfa1c84f3caf5b6fb2f0fe4df
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-12-29 08:18:26 +0000

    Small detail in a print statement

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from skillsData import candidateList
 def getFrozenApplicants(myList) : # myList is a list of regular sets, where each set represents a group of applicants that possess a required skill
   finalCut = myList[0] 
   for skill in range(0, len(myList) - 1):
-    finalCut.intersection_update(myList[skill + 1]) 
-  finalCut = frozenset(finalCut)
+    finalCut.intersection_update(myList[skill + 1]) #intersection between the sets
+  finalCut = frozenset(finalCut) #frozen set of final intersection of all sets
   return finalCut
 
-print("Welcome Recruiter... you have 100 applicants to review today.")
+print("Welcome Recruiter... you have 50 applicants to review today.")
 
 print("Your candidates have been organized for you into overlapping sets.")
 print("... Where each set corresponds to one of four desired skills.")
@@ -21,9 +21,9 @@ sqlApplicants = set()
 cPlusPlusApplicants = set()
 
 for candidate in candidateList:
-  for i in range (0, len(candidate)):
+  for i in range (0, len(candidate)): #parses through candidate's list of skills
     if candidate[i] == "Python":
-      pythonApplicants.add(candidate[0])
+      pythonApplicants.add(candidate[0]) #add candidates with that skill to the set for that skill
     if candidate[i] == "TensorFlow":
       tensorFlowApplicants.add(candidate[0])
     if candidate[i] == "SQL":
